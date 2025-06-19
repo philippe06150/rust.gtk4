2ae3b7a0b5df3b0017cd800393132a7b03f94f14
Philippe <142155374+philippe06150@users.noreply.github.com>
2025-06-19 19:56:14 +0200

    Update action.rs

--- action.rs
+++ action.rs
@@ -18,7 +18,7 @@ fn main() -> gtk::glib::ExitCode {
     // css
     app.connect_startup(|app| {
         let provider = gtk::CssProvider::new();
-        provider.load_from_data(DEFINITION_CSS);
+        provider.load_from_string(DEFINITION_CSS);
         gtk::style_context_add_provider_for_display(
             &gdk::Display::default().expect("Could not connect to a display."),
             &provider,
